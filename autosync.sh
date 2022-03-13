ce1dd00cd7e26da9ead27a0388e0d9c59e9ec491
SummonHIM <2239760702@qq.com>
2022-03-13 18:45:25 +0800

    change

--- autosync.sh
+++ autosync.sh
@@ -4,11 +4,13 @@ if [ -n "$(git log gh-pages..origin/gh-pages --oneline)" ]; then
     echo 检测到更改，正在合并最新拉取的更改...
     git merge origin/gh-pages
     echo 正在编译 Jekyll 站点
+    sed -i 's/url: "https:\/\/summonhim.top"/url: "https:\/\/8bits.group"/g' _config.yml
+    sed -i 's/header_feature_image: http:\/\/www.dmoe.cc\/random.php/header_feature_image: assets\/img\/header\/triangular.jpeg/g' _config.yml
+    sed -i 's/chn_website_approve: false/chn_website_approve: true/g' _config.yml
     Jekyll build
     echo 正在复制站点到 /var/www/html
-    cp _site/* /var/www/html/*
-    echo 正在复制 BuildWith 到 /var/www/html
-    cp /var/www/work/buildwith/* /var/www/html/*
+    cp -r _site/* /var/www/html
+    cp -r /var/www/work/buildwith/* /var/www/html
 else
     echo 无需更新。
 fi
