6e264e28a92c3b917dad19016849ac4acc2283d3
SummonHIM <2239760702@qq.com>
2022-03-13 19:11:41 +0800

    title

--- autosync.sh
+++ autosync.sh
@@ -5,6 +5,8 @@ runSync() {
     rm -rf /var/www/html/*
     echo 正在编译 Jekyll 站点
     sed -i 's/url: "https:\/\/summonhim.top"/url: "https:\/\/8bits.group"/g' _config.yml
+    sed -i "s/title: SummonHIM's Logs/title: 8-Bits Group/g" _config.yml
+    sed -i "s/header_text: SummonHIM's Logs/header_text: 八点八位元/g" _config.yml
     sed -i 's/header_feature_image: http:\/\/www.dmoe.cc\/random.php/header_feature_image: assets\/img\/header\/triangular.jpeg/g' _config.yml
     sed -i 's/chn_website_approve: false/chn_website_approve: true/g' _config.yml
     jekyll build
