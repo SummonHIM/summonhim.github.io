f8ebdaca84665c3e03f23ddd4e29906f1e6d5dd4
SummonHIM <2239760702@qq.com>
2022-03-13 17:26:35 +0800

    autocomp

--- autosync.sh
+++ autosync.sh
@@ -1,8 +1,14 @@
 cd $(dirname $0)
 git fetch origin gh-pages
 if [ -n "$(git log gh-pages..origin/gh-pages --oneline)" ]; then
-    echo 检测到更改，正在更新...
+    echo 检测到更改，正在合并最新拉取的更改...
     git merge origin/gh-pages
+    echo 正在编译 Jekyll 站点
+    Jekyll build
+    echo 正在复制站点到 /var/www/html
+    cp _site/* /var/www/html/*
+    echo 正在复制 BuildWith 到 /var/www/html
+    cp /var/www/buildwith/* /var/www/html/*
 else
     echo 无需更新。
 fi
