0ee886e4de5553a5e8aba2d8dd5049ea711c4d4e
SummonHIM <2239760702@qq.com>
2022-03-13 17:30:29 +0800

    work

--- autosync.sh
+++ autosync.sh
@@ -8,7 +8,7 @@ if [ -n "$(git log gh-pages..origin/gh-pages --oneline)" ]; then
     echo 正在复制站点到 /var/www/html
     cp _site/* /var/www/html/*
     echo 正在复制 BuildWith 到 /var/www/html
-    cp /var/www/buildwith/* /var/www/html/*
+    cp /var/www/work/buildwith/* /var/www/html/*
 else
     echo 无需更新。
 fi
