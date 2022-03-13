72a022df3b5350d899c063b35fbec9fe864097ea
SummonHIM <2239760702@qq.com>
2022-03-13 19:00:40 +0800

    force

--- autosync.sh
+++ autosync.sh
@@ -1,7 +1,4 @@
-cd $(dirname $0)
-git fetch origin gh-pages
-if [ -n "$(git log gh-pages..origin/gh-pages --oneline)" ]; then
-    echo 检测到更改，正在合并最新拉取的更改...
+runSync() {
     git restore .
     git merge origin/gh-pages
     echo 正在编译 Jekyll 站点
@@ -12,6 +9,19 @@ if [ -n "$(git log gh-pages..origin/gh-pages --oneline)" ]; then
     echo 正在复制站点到 /var/www/html
     cp -r _site/* /var/www/html
     cp -r /var/www/work/buildwith/* /var/www/html
+}
+
+cd $(dirname $0)
+git fetch origin gh-pages
+
+if [ -n "$(git log gh-pages..origin/gh-pages --oneline)" ]; then
+    echo 检测到更改，正在合并最新拉取的更改...
+    runSync
 else
     echo 无需更新。
+fi
+
+if [ $1 == force ]; then
+    echo 已启用强制合并并编译。
+    runSync
 fi
