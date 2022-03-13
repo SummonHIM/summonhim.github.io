75f7f1f64b448b5ece453eedb6720668f2d2b598
SummonHIM <2239760702@qq.com>
2022-03-13 17:17:23 +0800

    sync

--- autosync.sh
+++ autosync.sh
@@ -1,5 +1,8 @@
 cd $(dirname $0)
 git fetch origin gh-pages
 if [ -n "$(git log gh-pages..origin/gh-pages --oneline)" ]; then
+    echo 检测到更改，正在更新...
     git merge origin/gh-pages
+else
+    echo 无需更新。
 fi
