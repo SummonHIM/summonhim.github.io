ad677692f97bee94a52f3df5e66d15911f27d1d6
SummonHIM <summonhim@summonhim.top>
2026-06-29 12:44:17 +0800

    feat: 使用Sky作为主色调

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,36 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
 import Aura from '@primeuix/themes/aura'
+import { definePreset } from '@primeuix/themes'
 
 import App from './App.vue'
 import router from './router'
 import './assets/style/index.css'
+
+const skyAura = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{sky.50}',
+      100: '{sky.100}',
+      200: '{sky.200}',
+      300: '{sky.300}',
+      400: '{sky.400}',
+      500: '{sky.500}',
+      600: '{sky.600}',
+      700: '{sky.700}',
+      800: '{sky.800}',
+      900: '{sky.900}',
+      950: '{sky.950}',
+    },
+  },
+})
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(PrimeVue, {
   theme: {
-    preset: Aura,
+    preset: skyAura,
   },
 })
 app.use(router)
