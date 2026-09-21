39e8a1a14332f50fc39583e61d5731c898f05ae8
SummonHIM <summonhim@summonhim.top>
2026-09-21 22:42:10 +0800

    perf: minify build output with astro-compress

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'astro/config'
+import compress from 'astro-compress'
 import { SITE } from './src/consts'
 
 export default defineConfig({
   site: SITE,
   output: 'static',
+  integrations: [
+    compress({
+      Logger: 1,
+    }),
+  ],
 })
