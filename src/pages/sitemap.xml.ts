c41d34f6737a5e0a118b1ccac042000611c064ff
SummonHIM <summonhim@summonhim.top>
2026-09-21 22:43:31 +0800

    perf: output compact single-line sitemap

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -17,11 +17,9 @@ function toUrl(path: string): string {
 export const GET: APIRoute = () => {
   const urls = Object.keys(pages).map(toUrl).sort()
 
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${urls.map((url) => `  <url><loc>${url}</loc></url>`).join('\n')}
-</urlset>
-`
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls
+    .map((url) => `<url><loc>${url}</loc></url>`)
+    .join('')}</urlset>`
 
   return new Response(sitemap, {
     headers: {
